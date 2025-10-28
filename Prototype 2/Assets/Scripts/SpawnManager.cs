925936182a2619cf4548c89efc7f5288dca04a69
Christian Johnson <christian7johnson@gmail.com>
2025-10-28 18:09:58 -0400

    create spawnManager
    spawnManager created to have new dogs every time the  Player presses S key

---
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = Random.Range(-20,20);
+    private float spawnPosZ = 20;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Vector3 spawnPos = new Vector3(spawnRangeX, 0, spawnPosZ);
+
+            Instantiate(animalPrefabs[animalIndex], new Vector3(Random.Range(-20,20), 0, 20), animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
