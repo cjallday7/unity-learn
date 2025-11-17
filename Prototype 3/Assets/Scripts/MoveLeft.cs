ab73cf3521b9a87528395ed077bd644c5238b25a
Christian Johnson <christian7johnson@gmail.com>
2025-11-17 15:54:54 -0500

    3.4 completion

--- Prototype 3/Assets/Scripts/MoveLeft.cs
+++ Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -19,5 +19,10 @@ public class MoveLeft : MonoBehaviour
         {
             transform.Translate(Vector3.left * speed * Time.deltaTime);
         }
+
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
