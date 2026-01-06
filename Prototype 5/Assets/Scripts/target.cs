3443a1d3c4a3811f288f6886e0fef40de08b5930
Christian Johnson <christian7johnson@gmail.com>
2026-01-05 19:21:00 -0500

    Update target.cs

--- Prototype 5/Assets/Scripts/target.cs
+++ Prototype 5/Assets/Scripts/target.cs
@@ -2,10 +2,37 @@ using UnityEngine;
 
 public class target : MonoBehaviour
 {
+    private Rigidbody targetRb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        targetRb = GetComponent<Rigidbody>();
+
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        transform.position = new Vector3(RandomSpawnPos());
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque));
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
 
     // Update is called once per frame
